0b1b87c04765d5742011698a4e62c7223c8894e0
Enzo Victor Stanganelli Gama <64747700+Stanganelli@users.noreply.github.com>
2023-08-30 14:48:26 -0300

    Update Window.kt

--- Window.kt
+++ Window.kt
@@ -81,7 +81,7 @@ fun main() {
     arqBash.writeText("@\"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin\"\n\n" +
             "choco install -y python3 \n" +
             "python --version \n" +
-            ""
+            "cd "
     )
 
 }
